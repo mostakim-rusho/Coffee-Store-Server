6e612bc21167b69f8277c157670f07c98b5891e3
rusho <anikhabib62@gmail.com>
2025-07-15 18:24:48 +0600

    get and delte operation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
@@ -33,6 +33,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/coffee/:id", async (req, res) => {
+        const id =req.params.id;
+      const query ={_id: new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body;
@@ -40,6 +46,12 @@ async function run() {
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
     });
+    app.delete("/coffee/:id",async (req, res) => {
+      const id =req.params.id;
+      const query ={_id: new ObjectId(id)};
+      const result =await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
