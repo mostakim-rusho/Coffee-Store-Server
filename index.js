82a8fbd9000beeda5ab26b72355c38c3793f1fd2
rusho <anikhabib62@gmail.com>
2025-07-15 23:50:59 +0600

    asdded

--- index.js
+++ index.js
@@ -33,9 +33,28 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee =req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+      const result =await coffeeCollection.updateOne(filter, coffee, options)
+      res.send(result);
+    });
     app.get("/coffee/:id", async (req, res) => {
-        const id =req.params.id;
-      const query ={_id: new ObjectId(id)};
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.findOne(query);
       res.send(result);
     });
@@ -46,12 +65,12 @@ async function run() {
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
     });
-    app.delete("/coffee/:id",async (req, res) => {
-      const id =req.params.id;
-      const query ={_id: new ObjectId(id)};
-      const result =await coffeeCollection.deleteOne(query);
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
