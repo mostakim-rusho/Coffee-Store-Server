b230e6c1f3393acee57dd74719c3d85078f6c7d6
rusho <anikhabib62@gmail.com>
2025-07-18 02:25:14 +0600

    added

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     app.put("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
